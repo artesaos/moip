6c97122071070494db7a6a477ed31aa4adf1d1c5
The Black <jeancesargarcia@gmail.com>
2015-05-15 17:32:20 -0300

    fixed nomenclature comment

--- migrations/2015_03_11_021748_create_moip_table.php
+++ migrations/2015_03_11_021748_create_moip_table.php
@@ -14,7 +14,7 @@ class CreateMoipTable extends Migration {
 	{
 		Schema::create('moip', function(Blueprint $table) {
 			$table->increments('id');
-			$table->string('token', 32)->coment('Access token');
+			$table->string('token', 32)->comment('Access token');
 			$table->string('key', 40)->comment('Passkey');
 			$table->string('endpoint', 20)->comment('Performance environment');
 			$table->timestamps();
