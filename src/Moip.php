307e13d1df6315c1cf21a1b6b27e69428258d6b1
The Black <jeancesargarcia@gmail.com>
2015-05-18 18:53:31 -0300

    Added model Moip

--- src/Moip.php
+++ src/Moip.php
@@ -1,5 +1,8 @@
 <?php namespace Artesaos\Moip;
 
+# Aliases
+use App;
+
 /**
  * Responsible class by integration methods
  * 
@@ -9,5 +12,17 @@
  */
 class Moip
 {
+	/**
+	 * The table associated with the Moip model.
+	 *
+	 * @var string
+	 **/
+	private $moip;
 
+	/**
+	 * Initialize class
+	 */
+	public function __construct() {
+		$this->moip = App::make('Artesaos\Moip\Model\Moip');
+	}
 }
