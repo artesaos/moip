bf439d05e465889f5805b543aa92ded69c35655a
Jean C. Garcia <jeancesargarcia@gmail.com>
2015-09-18 20:18:02 -0300

    initialize class constructor

--- src/Moip.php
+++ src/Moip.php
@@ -2,6 +2,24 @@
 
 namespace Artesaos\Moip;
 
+use Illuminate\Contracts\Foundation\Application;
+
 class Moip
 {
+	/**
+	 * The Laravel Application.
+	 *
+	 * @var \Illuminate\Contracts\Foundation\Application
+	 **/
+	private $app;
+
+	/**
+	 * Class constructor.
+	 * 
+	 * @param \Illuminate\Contracts\Foundation\Application $app The Laravel Application.
+	 */
+	public function __construct(Application $app) 
+	{
+		$this->app = $app;
+	}
 }
