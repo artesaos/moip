7c323ff8f312c2d8eb61a2cdc855273cad4bc6c4
Jean C. Garcia <jeancesargarcia@gmail.com>
2015-09-18 20:24:00 -0300

    finalized constructor

--- src/Moip.php
+++ src/Moip.php
@@ -3,6 +3,8 @@
 namespace Artesaos\Moip;
 
 use Illuminate\Contracts\Foundation\Application;
+use Moip\Moip as M;
+use Moip\MoipBasicAuth;
 
 class Moip
 {
@@ -14,6 +16,13 @@ class Moip
 	private $app;
 
 	/**
+	 * Class Moip sdk.
+	 *
+	 * @var \Moip\Moip
+	 **/
+	private $moip;
+
+	/**
 	 * Class constructor.
 	 * 
 	 * @param \Illuminate\Contracts\Foundation\Application $app The Laravel Application.
@@ -21,5 +30,16 @@ class Moip
 	public function __construct(Application $app) 
 	{
 		$this->app = $app;
+		$this->moip = $this->app->make(M::class, [$this->app->make(MoipBasicAuth::class, [config('moip.credentials.token'), config('moip.credentials.key')]), $this->getHomologated()]);
+	}
+
+	/**
+	 * Get endpoint of request.
+	 * 
+	 * @return \Moip\Moip::ENDPOINT_PRODUCTION|\Moip\Moip::ENDPOINT_SANDBOX
+	 */
+	private function getHomologated()
+	{
+		return config('moip.homologated') === true ? M::ENDPOINT_PRODUCTION : M::ENDPOINT_SANDBOX;
 	}
 }
